b4a2319ece15ca7546dc937bf16de08c94706d8d
DaisyLRay <daisyraymond@outlook.com>
2024-01-07 14:55:30 +0000

    final adjustment to forecast

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
 
-  return days[date.getDay()];
+  return days[date.getDay()+1];
 }
 
 function handleSearchSubmit(event) {
